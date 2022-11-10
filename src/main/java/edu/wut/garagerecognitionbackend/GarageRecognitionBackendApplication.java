1d94e4d16c6b18eb03f7665f204ee474faae2747
Deleter-D <867909454@qq.com>
2022-11-10 17:19:13 +0800

    update

---
+++ src/main/java/edu/wut/garagerecognitionbackend/GarageRecognitionBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.wut.garagerecognitionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageRecognitionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GarageRecognitionBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/wut/garagerecognitionbackend/GarageRecognitionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.wut.garagerecognitionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageRecognitionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
